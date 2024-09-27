041cb3c0c1a963f5053ab6560c9b287c832eccbb
Safi Hyder <safihaider0987@gmail.com>
2024-09-28 00:12:19 +0530

    Refactor App component and remove loading functionality

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import About from './screen/About/About'
 import Contact from './screen/Contact/Contact'
 import Feed from './screen/Feed/Feed'
 import Footer from './screen/Footer/Footer'
-import Loader from './components/Animations/Loader'
+// import Loader from './components/Animations/Loader'
 import './App.css'
 
 function App() {
@@ -13,16 +13,16 @@ function App() {
   const feed = useRef(null);
   const contact = useRef(null);
   const about = useRef(null);
-  const [loading, setLoading] = useState(true)
-  useEffect(() => {
-      // Simulate an API call
-      setTimeout(() => {
-          setLoading(false);
-      }, 4000);
-  }, []);
-  if (loading) {
-      return <div className='flex w-100 h-[100vh] items-center justify-center'><Loader /></div>
-  }
+  // const [loading, setLoading] = useState(true)
+  // useEffect(() => {
+  //     // Simulate an API call
+  //     setTimeout(() => {
+  //         setLoading(false);
+  //     }, 4000);
+  // }, []);
+  // if (loading) {
+  //     return <div className='flex w-100 h-[100vh] items-center justify-center'><Loader /></div>
+  // }
   // Function to scroll to the section
   const scrollToSection = (ref) => {
     ref.current.scrollIntoView({ behavior: 'smooth' });
