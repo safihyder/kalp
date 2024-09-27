d6b34dd3a0fe9587af51bb6becab856489fae68b
Safi Hyder <safihaider0987@gmail.com>
2024-09-28 00:03:31 +0530

    Refactor image paths in Navbar and Footer components

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -36,7 +36,7 @@ useEffect(() => {
                         <li onClick={() => scrollToSection(home)} className=' cursor-pointer'> {list.home}</li>
                         <li onClick={() => scrollToSection(feed)}  className=' cursor-pointer'>{list.feed}</li>
                         <li onClick={() => scrollToSection(home)}  className=' cursor-pointer'> 
-                     <Link to='/'><img src="./public/kalpLogo.png" className="w-10 h-10" alt="" /></Link> </li>
+                     <Link to='/'><img src="./kalplogo.png" className="w-10 h-10" alt="" /></Link> </li>
                         <li onClick={() => scrollToSection(contact)}  className=' cursor-pointer'>{list.contact}</li>
                         <li onClick={() => scrollToSection(about)}  className=' cursor-pointer'> {list.about}</li>
                         </ul>
